43d8692fa1be21af39d8c98a9e885a6be7a0a01d
Mikolaj <mikolajkarla@gmail.com>
2024-06-02 16:15:02 +0200

    dodanie funkcji getCriteria i bootstrapa do indexu

--- getCriteriaAndResult.py
+++ getCriteriaAndResult.py
@@ -1,1 +1,15 @@
-# tutaj beda funkcje getCriteria getResults itd.
+from g4f.client import Client
+
+client = Client(
+
+) 
+#występuje błąd async, jednak zwraca odpowiedź
+def getCriteria(question):
+    response = client.chat.completions.create(
+        model="gpt-4",
+        messages=[{"role": "user", "content": ('Podaj kryteria dla Large Languages Models, aby móc ocenić ich dobre i złe strony wyboru danego modelu, dla pytania:'+question)}]
+    )
+    return response.choices[0].message.content
+
+
+print (getCriteria("Szukam telefonu z dużym ekranem i mocną baterią."))
