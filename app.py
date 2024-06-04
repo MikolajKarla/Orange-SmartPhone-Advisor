f7d19a234281c480a19c2cb27c443fbd69ed9f5d
Mickey <mikolajkarla@gmail.com>
2024-06-04 18:35:35 +0200

    added tailwind

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template,request
 from waitress import serve
+import getCriteriaAndResult
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=["POST","GET"])
 def index():
     return render_template('index.html')
+@app.route('/getCriteria',methods=["POST"])
+def generateCriteria():
+    table = ""
+    print(request.form)
+    if request.method == 'POST':
+        table = getCriteriaAndResult.getCriteria(request.form["question"])
+        print(table)
+    return render_template('/index.html', table=table)
 
-#tutaj bedzie routing 
 
 if __name__ == "__main__":
     app.run(app,port=8000)
--- getCriteriaAndResult.py
+++ getCriteriaAndResult.py
@@ -1,5 +1,7 @@
 from g4f.client import Client
 from g4f.Provider import Bing
+from bs4 import BeautifulSoup
+
 
 client = Client(
 ) 
@@ -11,18 +13,27 @@ def getCriteria(question):
         provider=Bing,
         messages=[{"role": "user", "content": ('Podaj 4-7 kryteriów dla Large Languages Models, aby móc ocenić ich dobre i złe strony wyboru danego modelu językowego. Podaj według formatu: <table class="border"><tr class="border"><td class="border"><strong> kryterium 1:</strong></td><td  class="border"><strong>kryterium 2:</strong></td><td class="border"><strong> kryterium 3:</strong></td><td  class="border"><strong>kryterium 4:</strong></td></tr><tr><td class="border">opis kryterium</td><td class="border">opis kryterium</td><td class="border">opis kryterium</td><td class="border">opis kryterium</td></tr></table>.Będzie on udostępniony na stronie, nie pisz nic poza tą tabelą.Oto pytanie:'+question)}]
     )
-    getResults(response.choices[0].message.content)
-    return response.choices[0].message.content
+    # getResults(response.choices[0].message.content)
+    soup = BeautifulSoup(response.choices[0].message.content, 'html.parser')
+    for tag in soup.find_all(True):
+        if tag.name != 'table' and tag.name != 'tr' and tag.name != 'td':
+            tag.decompose()
+        return soup
     
 
 
-def getResults(question):
-    response = client.chat.completions.create(
-        model="gpt-4-turbo",
-        provider=Bing,
-        messages=[{"role": "user", "content": ('Podaj 4-8 modeli językowych, które będa spełniać te kryteria najlepiej i wypisz je w następnych kolumnach:'+question)}]
-    )
-    print( response.choices[0].message.content)
+# def getResults(question):
+#     response = client.chat.completions.create(
+#         model="gpt-4-turbo",
+#         provider=Bing,
+#         messages=[{"role": "user", "content": ('Podaj 4-8 modeli językowych, które będa spełniać te kryteria najlepiej i wypisz je w następnych kolumnach:'+question)}]
+#     )
+#     print( response.choices[0].message.content)
 
 
-print (getCriteria("Szukam telefonu z dużym ekranem i mocną baterią."))
+
+if __name__ == '__main__':
+    try:
+        getCriteria()
+    except():
+        print(Exception)
